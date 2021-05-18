24184ceba78362bccb1298553518078be97cc3ff
Edson Nascimento <aquenzitech@gmail.com>
2021-05-18 12:40:44 +1000

    added menu class

--- menu.rb
+++ menu.rb
@@ -1,2 +1,13 @@
+require_relative './ingredient.rb'
 class Menu
+  
+  def initialize
+    @ingredients = []
+  end
+
+  def add_ingredient(name, price)
+    ingredient = Ingredient.new(name, price)
+    @ingredients << ingredient
+  end
+
 end
