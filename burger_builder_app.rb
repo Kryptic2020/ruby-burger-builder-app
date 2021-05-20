25f5707b40553c2e71c8600c1feb775ffb227b0e
Edson Nascimento <aquenzitech@gmail.com>
2021-05-20 15:59:56 +1000

    added error handling to the user input

--- burger_builder_app.rb
+++ burger_builder_app.rb
@@ -33,10 +33,27 @@ class Burger
     @order.get_ingredients.each do |name, quantity|    
       total += @menu.get_price(name) * quantity
     end
-    print TTY::Box.frame " Custom Burger Order Price ... $ #{total.round(2)} ".light_yellow.on_blue
-    puts
-    
+    #print TTY::Box.frame " Custom Burger Order Price ... $ #{total.round(2)} ".light_yellow.on_blue 
+    message_info(" Custom Burger Order Price ... $ #{total.round(2)} ")    
   end
+
+  def message_warn(message)
+    print TTY::Box.warn message.light_white.on_light_blue
+  end
+
+  def message_success(message)
+    print TTY::Box.success message.light_white.on_light_blue
+  end
+
+  def message_frame(message)
+    print TTY::Box.frame message.light_white.on_light_blue
+  end
+
+  def message_info(message)
+    print TTY::Box.info message.light_white.on_light_blue
+  end
+
+
 
   
 
--- ruby_burger_builder.rb
+++ ruby_burger_builder.rb
@@ -5,6 +5,12 @@ require "colorize"
 require 'colorized_string'
 prompt = TTY::Prompt.new
 
+# Create class for handling error when input is not a valid 
+class EmptyInputError < StandardError
+end
+class WrongInputError < StandardError
+end
+
 # Create an instance of Burger
 ingredients = {"fish" => 2.00,"chicken" => 2.00,"salad" => 0.70,"tomato" => 2.00, "cheese" => 1.50, "bacon" => 2.50,"egg" => 1.20,"onion" => 2.10,}
 burger = Burger.new("Custom Burger", ingredients.sort.to_h)
@@ -18,10 +24,10 @@ choices.push(' Done '.light_white.on_light_blue)
 
 # Create a loop so the user can keep adding ingredient until he is done
 loop do
-  Gem.win_platform? ? (system "cls") : (system "clear")
+  #Gem.win_platform? ? (system "cls") : (system "clear")
 
   # Display Welcome message
-  puts TTY::Box.frame "Welcome to the #{burger.name} Builder App".light_white.on_light_blue
+  burger.message_frame "Welcome to the #{burger.name} Builder App"
 
   # Display Ingredient Price List
   burger.print_ingredients
@@ -32,32 +38,38 @@ loop do
   # Display Order Total Amount  
   burger.order.has_order ? burger.display_order_total_amount : nil 
   
-  # Display Ingredient Select Options    
-  ingredient_question = TTY::Box.frame "Which ingredient would you like to add to your Custom Burger?". 
-  light_white.on_light_blue
-  input = prompt.select(ingredient_question , choices,per_page: 50,symbols: { marker: "==>" })
+  # Display Ingredient Select Options  
+  input = prompt.select(burger.message_frame("Which ingredient would you like to add to your Custom Burger?") , choices,per_page: 50,symbols: { marker: "==>" })
 
   #Break the loop is user is done
   if input == ' Done '.light_white.on_light_blue
     Gem.win_platform? ? (system "cls") : (system "clear")
 
-    # Display token for collect and Pay
-    token_info = TTY::Box.warn "Your order number for collect & Pay is #{rand(36**8).to_s(36)}. Enjoy your Custom Burger!".light_white.on_blue
-    print burger.order.has_order ? token_info : nil
+    # Display token for collect and Pay    
+    print burger.order.has_order ? burger.message_warn("Your order number for collect & Pay is #{rand(36**8).to_s(36)}. Enjoy your Custom Burger!") : nil
     break
   end
 
   # Display quantity input request
-  puts TTY::Box.frame "How many would you like?".light_white.on_light_blue
-  quantity = gets.to_i
+  begin
+    burger.message_frame "How many would you like?"
+    quantity = gets.strip
 
-  # Add quantity to order
-  if quantity > 0
-    burger.add_to_order(input.downcase, quantity)
+    # Handling errors
+    raise EmptyInputError, "Empty input Error" if quantity == ""
+    raise WrongInputError, "wrong input Error" if quantity.to_i == 0 
+    rescue 
+    print "\r" + ("\e[A\e[K")
+    puts " Empty input is not a valid input, please insert only numbers greater than zero" if quantity == ""
+    puts " #{quantity} is not a valid input, please insert only numbers greater than zero" if quantity.to_i == 0 && quantity != ""
+    retry
   end
-  
+  # Adding ingredients to the order
+  if quantity
+    burger.add_to_order(input.downcase, quantity.to_i) 
+  end
+   
 end
 
-# Display Thanks message
- thanks  = TTY::Box.success  "Thank you for visiting our Burger Builder App!!!".light_white.on_light_blue
-puts burger.order.has_order ? nil : thanks
+# Display Thanks message 
+burger.order.has_order ? nil : burger.message_success("Thank you for visiting our Burger Builder App!!!")
